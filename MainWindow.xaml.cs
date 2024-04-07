b30c64a7e55e8422060460943ca3de208fad57be
Toupae <ikrall001@gmail.com>
2024-04-07 15:58:44 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Input; // Include for KeyEventArgs
+
+namespace GreenCrosshairOverlay
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // Assuming the overlay window size is 100x100 for the crosshair
+            this.Width = 100;
+            this.Height = 100;
+
+            // Center window on a 1920x1080 screen
+            this.Left = (1920 - this.Width) / 2;
+            this.Top = (1080 - this.Height) / 2;
+        }
+    }
+}
